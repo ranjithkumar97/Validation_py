01bb29d3e469c2023952a431a7ea799cc7274764
santhosh kumar <sandysanthosh101@gmail.com>
2019-01-24 23:18:06 +0530

    Working with iter

---
+++ venv/Conditions.py
@@ -0,0 +1,20 @@
+import platform
+
+x = dir(platform)
+print(x)
+print("validate")
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+    mystr = "banana"
+    myit = iter(mystr)
+
+    print(myit)
+    print(myit)
+    print(myit)
+    print(myit)
+    print(myit)
+    print(myit)
+    print("The end")
