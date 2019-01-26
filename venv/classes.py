e291e64e2f2d2db2bd7e1ce031ef97a1436a72d2
santhosh kumar <sandysanthosh101@gmail.com>
2019-01-26 23:59:08 +0530

    Working with task

---
+++ venv/classes.py
@@ -0,0 +1,14 @@
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+
+    print(thisdict+" "+ fruits)
